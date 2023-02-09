57dee9a0b6f8cdcbb884706a7b9ad7776f9dce7f
SaifuddinKhan <sfddin.khan@gmail.com>
2023-02-09 22:28:16 +0530

    no comments

--- site/lib/__tests__/handlers.test.js
+++ site/lib/__tests__/handlers.test.js
@@ -5,7 +5,7 @@ test('Testing handlers', () => {
   const res = { render: jest.fn() };
   handler.home(req, res);
   expect(res.render.mock.calls.length).toBe(1);
-  expect(res.render).toBe('home');
+  expect(res.render.mock.calls[0][0]).toBe('home');
 });
 
 test('Testing 404 not found', () => {
@@ -44,16 +44,3 @@ test('About page is provided a fortune', () => {
   expect(res.render.mock.calls[0][0]).toBe('about');
   expect(res.render.mock.calls[0][1]).toEqual(expect.objectContaining({ fortune: expect.stringMatching(/\W/) }));
 });
-
-/*
-test('about page renders with fortune', () => {
-  const req = {};
-  const res = { render: jest.fn() };
-  handler.about(req, res);
-  expect(res.render.mock.calls.length).toBe(1);
-  expect(res.render.mock.calls[0][0]).toBe('about');
-  expect(res.render.mock.calls[0][1])
-    .toEqual(expect.objectContaining({
-      fortune: expect.stringMatching(/\W/)
-    }));
-}); */
