6b59cbf6b1cc5b437ae5894136c59bb0eb0e4e28
SaifuddinKhan <sfddin.khan@gmail.com>
2023-02-16 20:07:12 +0530

    added json req body parser to meadowlark.js, created two new sign up forms, using server-sided handling and fetch()/json. Uses redirects and currently only logs received form content.

--- meadowlark.js
+++ meadowlark.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { engine } = require('express-handlebars');
 const path = require('path');
+const bodyParser = require('body-parser');
 const port = process.env.PORT || 3000;
 
 const handler = require('./lib/handlers');
@@ -22,25 +23,29 @@ app.engine('handlebars', engine({
 }));
 
 app.set('view engine', 'handlebars');
-
 app.set('view cache', true);
 
 app.use(express.static(path.join(__dirname, '/public')));
-
+app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use(weathermiddle);
 
 app.get('/', handler.home);
-
 app.get('/about', handler.about);
-
-app.get('/about/services', handler.about_services);
-
+app.get('/about/services', handler.aboutServices);
+app.get('/custom', handler.custom);
 app.get('/headers', handler.headers);
-
+app.get('/newsletter-signup', handler.newsletterSignup);
+app.get('/newsletter-signup-fetch', handler.newsletterSignupFetch);
+app.get('/newsletter-signup-thankyou', handler.newsletterSignupThankyou);
 app.get('/weather', handler.weatherPage);
 
-app.use(handler.notfound);
+app.post('/newsletter-signup/process', handler.newsletterSignupProcess);
+app.post('/api/newsletter-signup', handler.api.newsletterSignup);
 
+app.use(handler.notfound);
 app.use(handler.servererror);
 
 if (require.main === module) {
